134093c5242644b0032dc568ec0972d9c0a93114
Jason Baer <jasonbaer16@gmail.com>
2026-01-02 10:28:21 -0500

    Initial Commit

---
+++ Index/IndexApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndexApp.swift
+//  Index
+//
+//  Created by Jason Baer on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct IndexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IndexTests/IndexTests.swift
@@ -0,0 +1,17 @@
+//
+//  IndexTests.swift
+//  IndexTests
+//
+//  Created by Jason Baer on 1/2/26.
+//
+
+import Testing
+@testable import Index
+
+struct IndexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
